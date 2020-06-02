a9b2ac9b8044691c0f078de9148e23ecb17cfc49
ozan şahin <ozan.sahin@prodea.com.tr>
2020-06-02 13:53:15 +0000

    ilk commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("SezaNakilSureci.SezaNakilSureci.controller.View1", {
+		onInit: function () {
+		/*	this.getView().setModel(this.getOwnerComponent().getModel("hr"));
+			this.getRouter().getRoute("View1").attachPatternMatched(this._onObjectMatched, this);
+			this.getRouter().getRoute("View1").attachPatternMatched(this._onObjectMatched, this);
+	*/	}
+	});
+});
